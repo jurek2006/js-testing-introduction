4fca5271855317b5ddb40cc755b635b60e7841ac
Jurek <jurek2006@gmail.com>
2018-10-11 15:54:51 +0200

    e2e tests

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require("puppeteer");
 const { generateText, checkAndGenerate } = require("./util");
 
 // UNIT TESTS
@@ -23,4 +24,24 @@ test("should return false for invalid data", () => {
     expect(text).toBe(false);
     const text2 = checkAndGenerate("Max");
     expect(text2).toBe(false);
+});
+
+// e2e TESTS
+test("should create an element with text and correct class", async () => {
+    const browser = await puppeteer.launch({
+        headless: true
+        // slowMo: 80,
+        // args: ["--window-size=1920,1080"]
+    });
+    const page = await browser.newPage();
+    await page.goto(
+        "file:///home/jurek/Dokumenty/PROGRAMOWANIE/PROJEKTY/js-testing-introduction/index.html"
+    );
+    await page.click("input#name");
+    await page.type("input#name", "Anna");
+    await page.click("input#age");
+    await page.type("input#age", "38");
+    await page.click("button#btnAddUser");
+    const finalText = await page.$eval(".user-item", el => el.textContent);
+    expect(finalText).toBe("Anna (38 years old)");
 });
